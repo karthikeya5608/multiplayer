bf33eff2e36fffce638064df62e40e2daa17e487
karthikeya5608 <72339198+karthikeya5608@users.noreply.github.com>
2020-12-31 16:05:35 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,45 @@
+var ball,database,position;
+
+function setup(){
+    database=firebase.database();
+    createCanvas(500,500);
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+    var ballposition=database.ref('ball/position');
+    ballposition.on("value",readposition,showerror);
+}
+
+function draw(){
+    background("white");
+    if(position!==undefined){
+    if(keyDown(LEFT_ARROW)){
+        changePosition(-3,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        changePosition(3,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        changePosition(0,-3);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        changePosition(0,+5);
+    }
+    drawSprites();
+}}
+
+function changePosition(x,y){
+    database.ref('ball/position').set({
+    'x' : position.x + x,
+    'y' : position.y + y,
+})}
+
+function readposition(data)
+{
+    position=data.val();
+    console.log(position.x);
+    ball.x=position.x;
+    ball.y=position.y;
+}function showerror()
+{
+    console.log("there is an error")
+}
